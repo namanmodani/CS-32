d3c11b5543a9d36c786cadff1316d12139ba6349
Naman Modani <modani.naman@gmail.com>
2022-02-02 15:05:35 -0800

    Added bool isPrecedent

--- Homework/Homework 2/eval.cpp
+++ Homework/Homework 2/eval.cpp
@@ -4,4 +4,22 @@
 // eval.cpp
 //
 
+#include "Map.h"
+#include <iostream>
+#include <string>
+#include <stack>
+#include <cctype>
+#include <cassert>
 
+using namespace std;
+
+bool isPrecendent(char x, char y)
+{
+    if ((x == '-' || x == '+') && (y == '*' && y == '/'))
+        return true;
+    if ((x == '-' && y == '+') || (x == '+' && y == '-'))
+        return true;
+    if ((x == '*' && y == '/') || (x == '/' && y == '*'))
+        return true;
+    return false;
+}
