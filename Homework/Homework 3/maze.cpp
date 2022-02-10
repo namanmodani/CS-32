0a6d53f69f4e8d041e7f7ebfc1df3a7b4532c43c
Naman Modani <modani.naman@gmail.com>
2022-02-09 22:37:16 -0800

    Vetted Problem 3

--- Homework/Homework 3/maze.cpp
+++ Homework/Homework 3/maze.cpp
@@ -25,19 +25,31 @@ bool pathExists(string maze[], int nRows, int nCols, int sr, int sc, int er, int
 {
     if (sr == er && sc == ec)
         return true;
-    maze[sr][sc] = "!";
+    maze[sr][sc] = "*";
 
     // North
-    if (maze[sr - 1][sc] == '.' && pathExists(maze, nRows, nCols, sr - 1, sc, er, ec))
-        return true;
+    if (sr > 0 && maze[sr - 1][sc] == '.')
+    {
+        if (pathExists(maze, nRows, nCols, sr - 1, sc, er, ec))
+            return true;
+    }
     // South
-    if (maze[sr + 1][sc] == '.' && pathExists(maze, nRows, nCols, sr + 1, sc, er, ec))
-        return true;
+    if (sr < nRows - 1 && maze[sr + 1][sc] == '.')
+    {
+        if (pathExists(maze, nRows, nCols, sr + 1, sc, er, ec))
+            return true;
+    }
     // East
-    if (maze[sr][sc + 1] == '.' && pathExists(maze, nRows, nCols, sr, sc + 1, er, ec))
-        return true;
+    if (sc < nCols - 1 && maze[sr][sc + 1] == '.')
+    {
+        if (pathExists(maze, nRows, nCols, sr, sc + 1, er, ec))
+            return true;
+    }
     // West
-    if (maze[sr][sc - 1] == '.' && pathExists(maze, nRows, nCols, sr, sc - 1, er, ec))
-        return true;
+    if (sc > 0 && maze[sr][sc - 1] == '.')
+    {
+        if (pathExists(maze, nRows, nCols, sr, sc - 1, er, ec))
+            return true;
+    }
     return false;
 }
